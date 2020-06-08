8d6a47b78883abb5de37785021a4726adab8ce3f
ycc <yangcc@lenovo.com>
2020-06-08 13:58:27 +0800

    first commit

---
+++ build/webpack.config.js
@@ -0,0 +1,10 @@
+'use strict'
+const path = require('path');
+module.exports = {
+    mode:'production',
+    entry:path.resolve(__dirname,'../src/index.js'),
+    output:{
+        path: path.resolve(__dirname,"../dist"),
+        filename: 'bundle.js',
+    }
+}
